bc5688a3ba880c50cab1b2eba947542f0106f2ea
lilsteviebird <stevenli880@gmail.com>
2022-04-09 01:47:52 -0400

    added read table functionality, added lambda function, fixed bug with infinite loop, need to look at why timer math is wrong

--- src/App.js
+++ src/App.js
@@ -1,17 +1,32 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import NewCard from './components/NewCard';
 import Cards from './components/Cards';
 
+import axios from 'axios';
+
+
 function App() {
 
-  const [cards, setCards] = useState([]);
+  let card_data = [];
+
+  const [cards, setCards] = useState(card_data);
+  const [value, setValue] = useState('');
+
+  useEffect(() => {
+    axios.get(`https://eeunxk9lxi.execute-api.us-east-1.amazonaws.com/default`)
+      .then(res => {
+        console.log(res.data.body);
+        setCards(res.data.body);
+      })
+  }, [value]);
 
   const addCardHandler = (card) =>{
     setCards( (prevCards) => {
       return [card, ...prevCards]
     });
   };
+
 
   return (
     <div className="App">
