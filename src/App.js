4472b50258549db5d68480845b354e861cdd66ac
lilsteviebird <stevenli880@gmail.com>
2022-04-08 21:21:52 -0400

    added card functionality DID NOT TEST YET

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import NewCard from './components/NewCard';
+import Cards from './components/Cards';
 
 function App() {
+
+  const [cards, setCards] = useState([]);
+
+  const addCardHandler = (card) =>{
+    setCards( (prevCards) => {
+      return [card, ...prevCards]
+    });
+  };
+
   return (
     <div className="App">
-      <NewCard />
+      <NewCard onAddCard = { addCardHandler } />
+      <Cards data = { cards }/>
     </div>
   );
 }
---
+++ src/components/Cards.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+import Timer from './Timer';
+
+import Card from 'react-bootstrap/Card';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+
+
+
+
+const Cards = (props) =>{
+    return(
+        <div>
+        {props.data.length === 0 ? <p>No Inthat Timeouts Found!</p> : 
+          <Row xs={1} md={2} className="g-4">
+          {props.data.map((card) => (
+            <Col>
+              <Card>
+                <Card.Body>
+                  <Card.Title>{card.Name}</Card.Title>
+                  <Timer Date = { card.date }/>
+                </Card.Body>
+              </Card>
+            </Col>
+          ))}
+        </Row>}
+        </div>
+    );
+}
+
+export default Cards;
